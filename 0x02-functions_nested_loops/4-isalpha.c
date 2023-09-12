41c7c66735df46964d3b346b8cf9047f90fd6fd4
SandyAbasman <abasifrekesandy@gmail.com>
2023-09-12 13:12:27 -0700

    is_alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - Checks for alphabetic order
+ *
+ * @c: alpabet to check for
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
