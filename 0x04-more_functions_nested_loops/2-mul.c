210218927e219646f3b6ae3f7f2d6c5871acdb95
SandyAbasman <abasifrekesandy@gmail.com>
2023-09-15 03:18:40 -0700

    multipies a,b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - int mul multplies two integers
+ *
+ *@a: interger to be multiplied
+ *@b: interger to be multiplied
+ *
+ * Return: answer of a * b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
