d9a60a80849deb1a2b1d4b2009beda18f68218d5
SandyAbasman <abasifrekesandy@gmail.com>
2023-09-15 21:05:02 -0700

    more

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,5 +10,8 @@ int _putchar(char i);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
